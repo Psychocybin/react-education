701e2e1690366c17acdac3e77845f7887fb2432c
GeorgiGeorgiev <boyanov81gg@gmail.com>
2023-05-29 12:15:50 +0300

    add input and use event

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,16 +3,23 @@ import SinglePurpose from "./SinglePurpose";
 
 function Counter(props) {
     let [count, setCount] = React.useState(0);
+    let [name, setName] = React.useState('');
+
+    const inputChangeHandler = (e) => {
+        setName(e.target.value);
+    };
 
     return(
         <>
-        <h2>Counter</h2>
+        <h2>Counter - {name}</h2>
 
         <ul>
             <SinglePurpose>{count}</SinglePurpose>
         </ul>
 
         <button onClick={() => setCount(count + 1)}>Add number</button>
+
+        <p><input type="text" onChange={inputChangeHandler} /></p>
         </>
     );
 }
