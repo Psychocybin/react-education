3b6ab81813fe572288a9806dd6373c517685776f
GeorgiGeorgiev <boyanov81gg@gmail.com>
2023-06-01 13:55:39 +0300

    refactoring code and use module.css

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,16 +1,23 @@
-import { useEffect } from 'react';
+//import { useEffect } from 'react';
+import styles from './TodoItem.module.css';
 
 export default function TodoItem({
     todo,
-    onDelete
+    onDelete,
+    onClick
 }) {
-    useEffect(() => {
-        console.log(`${todo.id} - mounted`);
+    // useEffect(() => {
+    //     console.log(`${todo.id} - mounted`);
 
-        return() => {
-            console.log(`${todo.id} - unmounted`);
-        }
-    }, [todo.id]);
+    //     return() => {
+    //         console.log(`${todo.id} - unmounted`);
+    //     }
+    // }, [todo.id]);
 
-    return <li>{todo.text} <button onClick={() => onDelete(todo.id)}>x</button></li>
+    return (
+        <li onClick={() => onClick(todo.id)} className={styles.todoItem}>
+            {todo.text}
+            <button onClick={() => onDelete(todo.id)}>x</button>
+        </li>
+    );
 };
--- src/components/TodoList2.js
+++ src/components/TodoList2.js
@@ -4,16 +4,17 @@ import TodoItem from "./TodoItem";
 
 export default function TodoList2() {
     const [todos, setTodos] = useState([
-        {id: 1, text: 'Party for birthday'},
-        {id: 2, text: 'Menu for parents'},
-        {id: 3, text: 'Menu for friends'},
-        {id: 4, text: 'The bill'},
+        {id: uniqid(), text: 'Party for birthday', isDone: false},
+        {id: uniqid(), text: 'Menu for parents', isDone: false},
+        {id: uniqid(), text: 'Menu for friends', isDone: false},
+        {id: uniqid(), text: 'The bill', isDone: false},
     ]);
  
     const onTodoInputBlur = (e) => {
         let todo = {
             id: uniqid(),
             text: e.target.value,
+            isDone: false,
         };
 
         setTodos(oldTodos => [
@@ -26,10 +27,41 @@ export default function TodoList2() {
     //     console.log('Mounted!');
     // }, []);
 
-    console.log('update');
+    //console.log('update');
 
     const deleteTodoItemClickHandler = (id) => {
         setTodos(oldTodos => oldTodos.filter(todo => todo.id !== id));
+    };
+
+    // const toggleTodoItemClickHandler = (id) => {
+    //     let currentTodo;
+    //     let currentIndex;
+
+    //     for (let i = 0; i < todos.length; i++) {
+    //         if (todos[i].id == id) {
+    //             currentTodo = todos[i];
+    //             currentIndex = i;
+
+    //             break;
+    //         }
+    //     }
+
+    //     let toggledTodo = {...currentTodo, isDone: !currentTodo.isDone};
+    //     setTodos(oldTodos => [
+    //         ...oldTodos.slice(0, currentIndex),
+    //         toggledTodo,
+    //         ...oldTodos.slice(currentIndex + 1)
+    //     ]);
+    // };
+
+    const toggleTodoItemClickHandler = (id) => {
+        setTodos(oldTodos => {
+            let selectedTodo = oldTodos.find(x => x.id === id);
+            let toggledTodo = {...selectedTodo, isDone: !selectedTodo.isDone};
+            let restTodos = oldTodos.filter(x => x.id !== id);
+
+            return [...restTodos, toggledTodo];
+        });
     };
 
     return(
@@ -37,7 +69,14 @@ export default function TodoList2() {
             <label htmlFor="todo-name">Add todo</label>
             <input type="text" id="todo-name" onBlur={onTodoInputBlur} name="todo" />
             <ul>
-                {todos.map(todo => <TodoItem key={todo.id} todo={todo} onDelete={deleteTodoItemClickHandler} />)}
+                {todos.map(todo => 
+                    <TodoItem 
+                        key={todo.id}
+                        todo={todo} 
+                        onDelete={deleteTodoItemClickHandler}
+                        onClick={toggleTodoItemClickHandler}
+                    />
+                )}
             </ul>
         </>
     );
